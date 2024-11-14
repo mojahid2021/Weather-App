5dbfca8a503f03767e37ed4ec62699d81e71bcc6
Md Mojahid <aammojahid@gmail.com>
2024-11-15 03:47:44 +0600

    Initial commit

---
+++ app/src/main/java/com/horizonhunters/weather/Constraint.java
@@ -0,0 +1,6 @@
+package com.horizonhunters.weather;
+
+public class Constraint {
+    public static String API_URL = "http://api.weatherapi.com/v1/";
+    public static String API_KEY = "071d4301869d479ca8a195713241411";
+}
